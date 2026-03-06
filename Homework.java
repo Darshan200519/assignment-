cc7e95272af15cb57d1139aafcddeb2d93c5dc5f
Darshan200519 <darshannarjinari50@gmail.com>
2026-03-06 14:23:58 +0530

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,103 @@
+package controlflow;
+
+public class Homework {
+	public static void main(String[] args) {
+		
+		
+		// 1. Check the number is Even or Odd
+		
+//		int num = 246 ;
+//		if (num %2 == 0) {
+//			System.out.println("The Given Number Is Even ");
+//		}
+//		else {
+//			System.out.println("The Given Number Is Odd");
+//		}
+		
+		// 2. Check person can eligible for voting
+		
+//		int age = 17 ;
+//		
+//		if (age >=18) {
+//			System.out.println("The Person can eligible for voting");
+//		}
+//		
+//		else {
+//			
+//			System.out.println("The Person Cannot eligible for voting");
+//		}
+//		
+		// 3. Check the Number is Positive or Negative 
+		
+//		int num = 2 ;
+//		if (num > 0) {
+//			System.out.println("The Number Is Positive");
+//		}
+//		else if (num < 0) {
+//			System.out.println("The Number Is Negative");
+//			
+//		
+//		}
+//		
+//		else {
+//			System.out.println("The Number Is Zero");
+//		}
+
+		
+		// 4.Write a program to compare two numbers and print the greater one
+//		int a = 7 , b = 6;
+//		if (a > b) {
+//			System.out.println("The A is greater than B" + a);
+//			
+//
+//		}
+//			else if (a < b) {
+//				System.out.println("The B is greater than A " + b);
+//			}
+//			else {
+//				System.out.println("The Both Number is  Same");
+//			}
+		//5.Check the grade of marks
+//		int score = 95;
+//
+//		if (score >= 90) {
+//		    System.out.println("Grade: A");
+//		} else if (score >= 80) {
+//		    System.out.println("Grade: B");
+//		} else {
+//		    System.out.println("Grade: C or below");
+//		}
+		
+		//6.Chek the Morning , afternoon , evening
+//		int hour = 18; // 2:00 PM
+//
+//		if (hour < 12) {
+//		    System.out.println("Good Morning!");
+//		} else if (hour < 18) {
+//		    System.out.println("Good Afternoon!");
+//		} else {
+//		    System.out.println("Good Evening!");
+//		}
+		// 7. Check the tempreature
+//		int temp = 14;
+//
+//		if (temp > 30) {
+//		    System.out.println("It's hot outside.");
+//		} else if (temp >= 15) {
+//		    System.out.println("The weather is pleasant.");
+//		} else {
+//		    System.out.println("It's cold outside.");
+//		}
+		// 8.Check the person is child , adult , yonger
+//		int age = 20;
+//
+//		if (age < 13) {
+//		    System.out.println("You are a child.");
+//		} else if (age < 20) {
+//		    System.out.println("You are a younger.");
+//		} else {
+//		    System.out.println("You are an adult.");
+//		}
+	}
+
+}
