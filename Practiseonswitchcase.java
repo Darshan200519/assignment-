ff678939027e8bc0d9f32b4a94994c83e6411c5c
Darshan200519 <darshannarjinari50@gmail.com>
2026-03-11 19:17:21 +0530

    Switch Case Practise Quetions Answers

---
+++ Practiseonswitchcase.java
@@ -0,0 +1,355 @@
+package controlflow;
+
+public class Practiseonswitchcase {
+
+	public static void main(String[] args) {
+//		1. Check Character Case
+//		char ch = 'a' ;
+//		switch (ch) {
+//		
+//		case 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+//        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' :
+//        	System.out.println("The Character is in Upper Case");
+//		break;
+//		case 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+//        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' :
+//        	System.out.println("The Character is in lower case");
+//		break;
+//		default :
+//			System.out.println("This is Not Chracter");
+//		}
+//		2.Check Weekday or Weekend
+//		String dayname = "Monday";
+//		switch (dayname) {
+//		
+//		case "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" :
+//			System.out.println("This is WeekDay");
+//		break;
+//		
+//		case "Saturday" , "Sunday" :
+//			System.out.println("This Weekend");
+//		default :
+//			System.out.println("Wong day or spelling enter");
+//		}
+		
+//		3.Check Arithmetic Operation Name
+		
+//		String op = "%";
+//		switch(op) {
+//		
+//		case "+" :
+//			System.out.println("Addition");
+//			break;
+//		case "-" :
+//			System.out.println("Addition");
+//			break;
+//		case "*" :
+//			System.out.println("Addition");
+//			break;
+//		case "/" :
+//			System.out.println("Addition");
+//			break;
+//		default :
+//			System.out.println("Invalid Opretion");
+//			
+//		}
+//		4.	Fruit Price Menu
+//		Write a Java program using switch case to display price of fruits.
+		
+//		String fruitname = "Apple";
+//		switch(fruitname) {
+//		case "Apple" :
+//			System.out.println("Apple--180rs per Kg");
+//			break;
+//		case "Mango" :
+//			System.out.println("Mango--80rs per Kg");
+//			break;
+//		case "Banana" :
+//			System.out.println("Banana--40rs per Kg");
+//			break;
+//		case "Orange" :
+//			System.out.println("Orange--60rs per Kg");
+//			break;
+//		default : 
+//			System.out.println("This fruit is not avialable");
+//		}
+//			5.Language Selection
+//Write a Java program using switch case to display greeting message.
+			
+//			String languagename = "Marathi";
+//			switch (languagename) {
+//			case "Marathi" :
+//				System.out.println("Namskar");
+//				break;
+//			case "English" :
+//				System.out.println("Hello");
+//				break;
+//			case "Hindi" :
+//				System.out.println("Namste");
+//				break;
+//			default :
+//				System.out.println("I cant understand this Langauage");
+//		}
+//		6. Student Result Status
+//		char grade ='F' ;
+//		switch(grade) {
+//		case 'A' :
+//			System.out.println("Disticntion");
+//			break;
+//		case 'B' :
+//			System.out.println("First Class");
+//			break;
+//		case 'C' :
+//			System.out.println("Second Class");
+//			break;
+//		case 'D' :
+//			System.out.println("Pass");
+//			break;
+//		case 'F' :
+//			System.out.println("_________Fail_________");
+//			break;
+//		default: 
+//			System.out.println("Worng Grade Enter");
+//		
+//		}
+//		7. Season Based on Month
+//Write a Java program using switch case to print season.
+//		String monthname = "January" ;
+//		switch (monthname) {
+//		
+//		case "December" , "January","February" :
+//			System.out.println("This is the winter session");
+//		break;
+//		case "March" , "April","May" :
+//			System.out.println("This is the summer session");
+//		break;
+//		case "june" , "july","august" :
+//			System.out.println("This is the monsoon session");
+//		break;
+//		case "sepetember" , "october","november" :
+//			System.out.println("This is the autumn session");
+//		break;
+//		default :
+//			System.out.println("This is Not Month------Please enter the valid month");	
+//		
+//		}
+//		8.ATM Menu
+
+//Write a Java program using switch case.
+		
+//		int choise = 5;
+//		switch (choise) {
+//		case 1:
+//			System.out.println("***Check The Balance***");
+//			break;
+//		case 2:
+//			System.out.println("***Withdraw The Money***");
+//			break;
+//		case 3:
+//			System.out.println("***Deposit The Money***");
+//			break;
+//		case 4 :
+//			System.out.println("***Exit***");
+//			break;
+//		default :
+//			System.out.println("Enter The Worng choise please try again");
+//		
+//		}
+//		9.Simple Food Menu
+
+//Write a Java program using switch case.
+//
+//1 → Pizza
+//2 → Burger
+//3 → Sandwich
+//4 → Pasta
+//		int choise = 4;
+//		switch (choise) {
+//		case 1:
+//			System.out.println("***Pizza***");
+//			break;
+//		case 2:
+//			System.out.println("***Burger***");
+//			break;
+//		case 3:
+//			System.out.println("***Sandwich***");
+//			break;
+//		case 4 :
+//			System.out.println("***Pasta***");
+//			break;
+//		default :
+//			System.out.println("Enter The Worng choise please try again");
+//		
+//		}
+//		10. Check Digit Name
+
+//Write a Java program using switch case to display digit name.
+//
+//Example:
+//0 → Zero
+//1 → One
+//2 → Two
+//…
+//9 → Nine
+//		int num = 11 ;
+//		switch(num) {
+//		case 1 :
+//			System.out.println("One");
+//			break;
+//		case 2 :
+//			System.out.println("Two");
+//			break;
+//		case 3 :
+//			System.out.println("Three");
+//			break;
+//		case 4 :
+//			System.out.println("Four");
+//			break;
+//		case 5 :
+//			System.out.println("Five");
+//			break;
+//		case 6 :
+//			System.out.println("Six");
+//			break;
+//		case 7 :
+//			System.out.println("Seven");
+//			break;
+//		case 8 :
+//			System.out.println("Eight");
+//			break;
+//		case 9 :
+//			System.out.println("Nine");
+//			break;
+//		case 10 :
+//			System.out.println("Ten");
+//			break;
+//		default :
+//			System.out.println("This Number is in not Dictionary");
+//			
+//		}
+//		11.. Area of Shapes
+
+//Write a Java program using switch case.
+//
+//1 → Square
+//2 → Circle
+//3 → Rectangle
+//		int num = 1 ;
+//		switch(num) {
+//		case 1 :
+//			System.out.println("Square");
+//			break;
+//		case 2 :
+//			System.out.println("Circle");
+//			break;
+//		case 3 :
+//			System.out.println("rectangle");
+//			break;
+//		default :
+//			System.out.println("This is Shape is not available");
+//	}
+//		12. Check Number Range
+//
+//		Write a Java program using switch case.
+//
+//		Example:
+//		Input: number / 10
+//
+//		0–3 → Low
+//		4–7 → Medium
+//		8–10 → High
+//		String num = "11" ;
+//		switch(num) {
+//		case "0" , "1", "2","3" :
+//			System.out.println("Low");
+//			break;
+//		case "4", "5","6", "7" :
+//			System.out.println("Medium");
+//			break;
+//		case "8","9" ,"10" :
+//			System.out.println("High");
+//			break;
+//		default :
+//			System.out.println("This is number is not available");
+//		
+//		}
+//		13. Company Department
+
+//		Write a Java program using switch case.
+//
+//		1 → HR
+//		2 → Finance
+//		3 → IT
+//		4 → Marketing
+//
+//		Print department name.
+//		int choise = 1 ;
+//		switch(choise) {
+//		case 1 :
+//			System.out.println("Hr");
+//			break;
+//		case 2 :
+//			System.out.println("Finance");
+//			break;
+//		case 3 :
+//			System.out.println("IT");
+//		case 4:
+//			System.out.println("Marketing");
+//		default :
+//			System.out.println("This department is Not avialalble");
+//			
+//		}
+//		14. Vehicle Type
+//
+//		Write a Java program using switch case.
+//
+//		1 → Bike
+//		2 → Car
+//		3 → Bus
+//		4 → Truck
+//
+//		Print vehicle type.
+//		int choise = 1 ;
+//		switch(choise) {
+//		case 1 :
+//			System.out.println("Bike");
+//			break;
+//		case 2 :
+//			System.out.println("Car");
+//			break;
+//	    case 3 :
+//		System.out.println("Bus");
+//		break;
+//	    case 4 :
+//			System.out.println("Truck");
+//			break;
+//			default :
+//				System.out.println("This vechile not availabale");
+//		}
+//		15. Check Password Strength Level
+//
+//		Write a Java program using switch case.
+//
+//		Input level number:
+//
+//		1 → Weak Password
+//		2 → Medium Password
+//		3 → Strong Password
+//		int choise = 1 ;
+//		switch(choise) {
+//		case 1 :
+//			System.out.println("Weak Password");
+//			break;
+//		case 2 :
+//			System.out.println("Medium Password");
+//			break;
+//		case 3 :
+//			System.out.println("Strong Password");
+//			break;
+//		default :
+//			System.out.println("Invalid chose");
+//		}
+		
+	}
+
+}
