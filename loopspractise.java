14b92c5fd0cbe7772201319e4a305f63157d32e4
Darshan200519 <darshannarjinari50@gmail.com>
2026-03-13 15:06:42 +0530

    Practise on Loops

---
+++ loopspractise.java
@@ -0,0 +1,63 @@
+package controlflow;
+
+public class loopspractise {
+	public static void main(String[] args) {
+//		1.Print numbers from 1 to 10 using a loop
+//		for(int i = 1; i<=10; i++) {
+//			System.out.println(i);
+//		}
+//		2.Print numbers from 10 to 1
+//		for(int i = 10; i>=1; i--) {
+//			System.out.println(i);
+//		}
+//		3.Print all even numbers between 1 and 50
+//		for(int i = 1; i<=50; i++) {
+//			if(i%2==0) {
+//				System.out.println(i);
+//			}
+//		}
+//		3.Print all odd numbers between 1 and 50.
+//		for(int i = 1; i<=50; i++) {
+//			if(i%2!=0) {
+//				System.out.println(i);
+//			}
+//		}
+//		4.Print the table of 5 using a loop
+//		for(int i=1; i<=10; i++) {
+//			System.out.println(5*i);
+//		}
+//		5.Print the table of any number (e.g., n = 7)
+//		for(int i=1; i<=10; i++) {
+//			int n = 10;
+//			System.out.println(n*i);
+//		}
+//		6.Print Hello 10 times.
+//		for(int i = 1; i <=10; i++) {
+//			System.out.println("Hello" + i);
+//		}
+//		7.Print the sum of first 10 natural numbers.
+//		int sum=0;
+//		for(int i=1; i<=10; i++) {
+//			sum += i;
+//			
+//		}
+//		System.out.println("Sum of the natrual numbers between 1 to 10 = " + sum);
+//		8.Print the sum of even numbers from 1 to 100
+//		int sum = 0;
+//		for(int i=1; i<=100; i++) {
+//			if(i%2 ==0 ) {
+//				sum += i;
+//			}
+//		}
+//		System.out.println("Sum of all even Numbers between 1 to 100 =" + sum);
+//		9.Print the sum of odd numbers from 1 to 100.
+//		int sum = 0;
+//		for(int i=1; i<=100; i++) {
+//			if(i%2 !=0 ) {
+//				sum += i;
+//			}
+//		}
+//		System.out.println("Sum of all Odd Numbers between 1 to 100 =" + sum);
+	}
+
+}
