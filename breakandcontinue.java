cac30871761b75664053c69c5a6f657f27cb1d83
Darshan200519 <darshannarjinari50@gmail.com>
2026-03-18 14:13:54 +0530

    Create breakandcontinue.java

---
+++ breakandcontinue.java
@@ -0,0 +1,74 @@
+package controlflow;
+
+public class breakcontinue {
+	public static void main(String[] args) {
+
+//		Home work break and continue
+//		1.Print numbers from 1 to 50 but stop when number becomes 25.
+//		for(int i=1; i<=50; i++) {
+//			if(i==25) {
+//				break;
+//			}
+//			System.out.println(i);
+		
+//		}
+//		2.Print even numbers from 1 to 100 but stop when you get first odd number.
+		
+//
+//		        for (int i = 1; i <= 100; i++) {
+//		            
+//		            if (i % 2 != 0) {
+//		                System.out.println("First odd number found =" + i  );
+//		                break;
+//		            } 
+//		        }
+//		        		            System.out.println("Suppose the fist odd number is 1 the no even numbers will be print");
+//		 3.Print table of 9 but stop when product becomes more than 50.
+//			int num = 9;
+//			for(int i=1; i<=10; i++) {
+//				int mul = num*i;
+//				if(mul>50) {
+//					
+//					break;
+//				}
+//				System.out.println(mul);
+//				
+//			}
+//		4.Print numbers from 1 to 50 but skip numbers divisible by 7.
+//		for (int i =1 ; i<=50; i++) {
+//			if(i%7 == 0) {
+//				continue;
+//		}
+//		System.out.println(i);
+//			
+//
+//		}	
+//		5.Print numbers from 1 to 100 but skip numbers ending with digit 5.
+		
+		        
+
+//		        for (int i = 1; i <= 100; i++) {
+//		           
+//		            if (i % 10 == 5) {
+//		                continue; 
+//		            }
+//
+//		            
+//		            System.out.print(i + " ");
+//		           
+//		        }
+//		6.Print characters from ‘A’ to ‘Z’ but skip vowels.
+//			for(char c='A'; c<='Z'; c++) {
+//				if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+//					continue;
+//				}
+//					
+//					System.out.println(c);
+//			
+//			}	
+		    }
+		
+		
+	}
+
+
