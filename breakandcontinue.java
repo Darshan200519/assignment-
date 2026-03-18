11a7774ab325e09e35f60073428cfb1f7a387e02
Darshan200519 <darshannarjinari50@gmail.com>
2026-03-18 14:40:37 +0530

    Update breakandcontinue.java

--- breakandcontinue.java
+++ breakandcontinue.java
@@ -66,9 +66,72 @@ public class breakcontinue {
 //					System.out.println(c);
 //			
 //			}	
+		
 		    }
 		
 		
 	}
+------------------------------------------------Iner Loop Outer Loop -------------------------------------------------------------
+	package controlflow;
+
+public class innerouterloop {
+	public static void main(String[] args) {
+//		1.
+//		        for (int i = 1; i <= 5; i++) {
+		               
+//		            for (int j = 1; j <= i; j++) {
+//		                System.out.print("* ");
+//		            }
+
+		            
+//		            System.out.println();
+//		        }
+//		 2.
+//		        for (int i = 5; i >= 1; i--) {
+//		               
+//		            for (int j = 1; j <= i; j++) {
+//		                System.out.print("* ");
+//		            }
+//
+//		            
+//		            System.out.println();
+//		        
+//		        }  
+//		3.
+//		 for (int i = 1; i <= 5; i++) {
+//             
+//          for (int j = 1; j <= 5; j++) {
+//              System.out.print("* ");
+//          }
+//
+//          
+//          System.out.println();
+//      
+//      } 
+//		 4.
+		 
+			        
+
+			        for (int i = 1; i <= 5; i++) {
+			            
+			            
+			            for (int j = 1; j <= 5 - i; j++) {
+			                System.out.print(" ");
+			            }
+
+			            
+			            for (int k = 1; k <= i; k++) {
+			                System.out.print("* ");
+			            }
+
+			           
+			            System.out.println();
+			        }
+			    }
+			}
+		
+		     
+	
+
 
 
